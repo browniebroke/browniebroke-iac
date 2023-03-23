355364596a671a0df2bc96655834a0544adce370
Bruno Alla <alla.brunoo@gmail.com>
2023-03-23 20:39:31 +0000

    Add repo from my blog

--- github/repos.tf
+++ github/repos.tf
@@ -8,9 +8,29 @@ resource "github_repository" "browniebroke_iac" {
   delete_branch_on_merge = true
   allow_update_branch    = true
 
-  has_issues      = true
-  has_discussions = true
-  has_projects    = false
-  has_wiki        = false
-  has_downloads   = false
+  has_issues           = true
+  has_discussions      = true
+  vulnerability_alerts = true
+  has_projects         = false
+  has_wiki             = false
+  has_downloads        = false
+}
+
+resource "github_repository" "browniebroke_dot_com" {
+  name         = "browniebroke.com"
+  topics       = ["blog", "gatsby", "gatsbyjs"]
+  description  = "Personal blog"
+  homepage_url = "https://browniebroke.com"
+
+  visibility = "public"
+
+  delete_branch_on_merge = true
+  allow_update_branch    = true
+
+  has_issues           = true
+  has_discussions      = false
+  vulnerability_alerts = true
+  has_projects         = false
+  has_wiki             = false
+  has_downloads        = false
 }
